1a07161a15ce2e930e5551598ea71f7c211fe87f
Ana Carolina Costa Pereira <carolina.cst.prr@gmail.com>
2025-11-13 14:50:21 -0300

    Nova Funcionalidade(Pessoa):
    Criei o arquivo .h para a classe pessoa

---
+++ Pessoa.h
@@ -0,0 +1,43 @@
+#ifndef PROJETO_FINAL_INF_112_PESSOA_H
+#define PROJETO_FINAL_INF_112_PESSOA_H
+
+#include <string>
+
+class Pessoa{
+
+    protected:
+        std::string nome; 
+        std::string cpf;
+        std::string senha;
+        std::string telefone;
+
+    public:
+        //Construtor e destrutor:
+        Pessoa(std::string nome, std::string cpf, std::string senha, std::string telefone);
+        ~Pessoa();
+        
+        //Gets
+        std::string getNome() const;
+        std::string getCpf() const;
+        std::string getSenha() const;
+        std::string getTelefone() const;
+        
+        //Sets
+        void setNome( std::string nome);
+        //CPF é uma informação "imutável", então não precisa de um método set()
+        void setSenha(std::string senha);
+        void setTelefone(std::string telefone);
+        
+        //Métodos
+        //Impressão dos dados do usuário:
+        virtual void vizualizaDados(); // Versão genérica com só os dados de pessoa 
+                                       // e vai ser redefinida nas subclasses, com os dados
+                                       // específicos de médico, atendente e cliente.
+        //Impressão dos agendamentos:
+        virtual void vizualizaAgendamentos()=0; //Virtual pura, só vai ser implementada nas subclasses 
+                                                // porque além do comportamento diferir entre as classes, não
+                                                // existem um genérico.
+
+};
+
+#endif //PROJETO_FINAL_INF_112_PESSOA_H
