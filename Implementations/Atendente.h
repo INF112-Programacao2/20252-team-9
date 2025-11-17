9763bcacf5b797cb1f76d9084c4be64ec73c1318
magesti1 <eduardo.santiago@ufv.br>
2025-11-17 13:55:00 -0300

    Começo da implementação de Atendente

--- Implementations/Atendente.h
+++ Implementations/Atendente.h
@@ -1,12 +1,13 @@
 #include "Headers/Atendente.h"
-#include <string>
-
-
+#include "Utils.h"
+#include <stdexcept>
 
 
 //Gets e Sets
 std::string Atendente::getMatricula() const {return matricula;}
 
 void Atendente::setMatricula(std::string nova_matricula) {
+    if(stringVazia(nova_matricula))
+        throw std::invalid_argument("Matricula Inválida");
     this->matricula = nova_matricula;
 }
