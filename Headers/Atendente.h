2e1319f24ef993f8c3a318aa89c7018848e9a96d
magesti1 <eduardo.santiago@ufv.br>
2025-11-17 13:46:22 -0300

    Alterações feitas durante a reunião

--- Headers/Atendente.h
+++ Headers/Atendente.h
@@ -27,7 +27,7 @@ class Atendente : public Pessoa{
         
         //Metodos
         void VizualizaDados() override;
-        void VizualizaAgendamentos() override; // Metodos herdados de pessoa
+        void VizualizaAgendamentos(Clinica &clinica) override; // Metodos herdados de pessoa
         void CadastrarMedico(Clinica &clinica);
         void DesligarMedico(Clinica &clinica);
         void MarcarAgendamento(Clinica &clinica); //Função que também pode ser executada pelo paciente
