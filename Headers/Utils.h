331bd056c90831c13d4d3894a779910b7f3d64ff
Ana Carolina Costa Pereira <carolina.cst.prr@gmail.com>
2025-11-28 20:22:22 -0300

    NovaFuncionalidade(2 funções auxiliares novas)
    somenteLetras() e somenteNumeros() para verificar as strings e ajudar na validação de entradas

--- Headers/Utils.h
+++ Headers/Utils.h
@@ -6,3 +6,9 @@
 
 //Função que retorna se é uma string vazia(Sem nenhum caracter/Vários espaços/ tabs etc...)
 bool stringVazia(const std::string &string);
+
+//Funcao que checa se um string é composto só por letras(validação do nome)
+bool somenteLetras(const std::string &palavra);
+
+//Função para checar se uma string só tem números(validação do nº de telefone
+bool somenteNumeros(const std::string& numero);
